530cc982e69e0d43c29670a27c8edcb63ee3ef9d
Likotan <806206110@qq.com>
2017-10-27 19:34:56 +0800

    liko
    上传代码

---
+++ baselibrary/src/main/java/com/liko/base/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.liko.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Liko
+ * @Date 17/10/27 下午7:33
+ * @Description 基类Activity
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
